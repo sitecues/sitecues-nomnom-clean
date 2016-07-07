bbe5c445ca6112c18e2ebb0787d9fdf2570913df
Aaron Leventhal <aleventhal@aisquared.com>
2016-07-07 16:57:01 -0400

    Allow --startDate=YYYYMMDD and --endDate=YYYYMMDD. First report is just the total number of entries.

--- bin/nomnom-cli.js
+++ bin/nomnom-cli.js
@@ -4,15 +4,18 @@
 
 // Crash and burn, die fast if a rejected promise is not caught.
 process.on('unhandledRejection', function (err) {
-    throw err;
+  throw err;
 });
 
 const
   constants = require('../lib/constants'),
-  severityOptions = Object.keys(constants.SEVERITY),
   config = require('yargs').usage('Usage: $0 <command> [options]')
     .describe('reports', 'Comma-separated lists of reports from lib/reports/ folder (default = all)')
     .string('reports')
+    .describe('startDate', 'Start date in the format YYYYMMDD')
+    .number('startDate')
+    .describe('endDate', 'End date in the format YYYYMMDD')
+    .number('endDate')
     .describe('view', 'Name of view to use, e.g. text|json|html (default = json)')
     .string('view')
     .help('h')
@@ -21,9 +24,10 @@ const
   reporter = require('../lib/reporter.js');
 
 reporter(config)
-  .then(function (rawReport) {
-    // TODO use winston library if we need more advanced logging
-    console.log('\n\nWriting report ...');
-    console.log(rawReport);
-    console.log('Finished');
+  .then(function (rawReports) {
+    console.log('\n\nWriting reports ...');
+    rawReports.forEach((rawReport) => {
+      console.log(rawReport);
+      console.log('Finished');
+    });
   });
